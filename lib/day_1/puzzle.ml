a6ebab5f83d8a20eeda1fa17c0c6ee9fd544801d
Pontus Nagy <pontusnagy@gmail.com>
2020-12-02 15:19:05 +0100

    Day 1a, chill af

--- lib/day_1/puzzle.ml
+++ lib/day_1/puzzle.ml
@@ -1,14 +1,22 @@
 (* ---- parse input --- *)
 
-let parsed_input =
-  Util.read_file "./lib/day_1/input" |> List.map ~f:int_of_string
+let input = "./lib/day_1/input"
+(* let input = "./lib/day_1/example" *)
+
+let parsed_input = input |> Util.read_file |> List.map ~f:int_of_string
 
 (* ---- puzzle_a ------ *)
 
-let puzzle_a =
-  parsed_input
-  |> List.fold_left ~f:( + ) ~init:0
-  |> Printf.printf "day_1_puzzle_a: %d\n%!"
+let rec solve_a numbers =
+  let rec solve' hd tl =
+    match tl with
+    | tl_hd :: tl_tl ->
+        if hd + tl_hd == 2020 then hd * tl_hd else solve' hd tl_tl
+    | [] -> solve_a (List.tl numbers)
+  in
+  match numbers with hd :: tl -> solve' hd tl | _ -> failwith "invalid input"
+
+let puzzle_a = parsed_input |> solve_a |> Printf.printf "day_1_puzzle_a: %d\n%!"
 
 (* ---- puzzle_b ------ *)
 
--- lib/day_BP/puzzle.ml
+++ lib/day_BP/puzzle.ml
@@ -1,14 +1,13 @@
 (* ---- parse input --- *)
 
-let parsed_input =
-  Util.read_file "./lib/day_BP/input" |> List.map ~f:int_of_string
+let input = "./lib/day_BP/input"
+(* let input = "./lib/day_BP/example" *)
+
+let parsed_input = input |> Util.read_file |> List.map ~f:int_of_string
 
 (* ---- puzzle_a ------ *)
 
-let puzzle_a =
-  parsed_input
-  |> List.fold_left ~f:( + ) ~init:0
-  |> Printf.printf "day_BP_puzzle_a: %d\n%!"
+let puzzle_a = 0 |> Printf.printf "day_BP_puzzle_a: %d\n%!"
 
 (* ---- puzzle_b ------ *)
 
